2add6d3be0ff8b230d6d78d211753efb6ba2a60e
shtwangy <shtwangy@gmail.com>
2020-12-13 22:52:34 +0900

    feat(app/app.js): add update user endpoint

--- app/app.js
+++ app/app.js
@@ -50,6 +50,20 @@ app.get('/api/v1/search', (req, res) => {
     db.close()
 })
 
+const run = async (sql, db, res, message) => {
+    return new Promise((resolve, reject) => {
+        db.run(sql, (err) => {
+            if (err) {
+                res.status(500).send(err)
+                return reject()
+            } else {
+                res.json({message: message})
+                return resolve()
+            }
+        })
+    })
+}
+
 // Create a new user
 app.post('/api/v1/users', async (req, res) => {
     const db = new sqlite3.Database(dbPath)
@@ -57,21 +71,28 @@ app.post('/api/v1/users', async (req, res) => {
     const profile = req.body.profile ? req.body.profile : ''
     const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ''
 
-    const run = async (sql) => {
-        return new Promise((resolve, reject) => {
-            db.run(sql, (err) => {
-                if (err) {
-                    res.status(500).send(err)
-                    return reject()
-                } else {
-                    res.json({message: '新規ユーザーの作成に成功しました！'})
-                    return resolve()
-                }
-            })
-        })
-    }
+    await run(`INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`, db, res, 'ユーザーの作成に成功しました')
+    db.close()
+})
 
-    await run(`INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`)
+// Update a new user
+app.put('/api/v1/users/:id', async (req, res) => {
+    const db = new sqlite3.Database(dbPath)
+    const id = req.params.id
+
+    // 現在のユーザー情報を取得する
+    db.get(`SELECT * FROM users WHERE id = ${id}`, async (err, row) => {
+        const name = req.body.name ? req.body.name : row.name
+        const profile = req.body.profile ? req.body.profile : row.profile
+        const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
+        await run(
+            `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
+            db,
+            res,
+            'ユーザー情報の更新に成功しました'
+        )
+    })
+
     db.close()
 })
 
