07b74ce20cbae8cba68550808740174940b74b9e
shtwangy <shtwangy@gmail.com>
2020-12-14 22:20:34 +0900

    feat(app/public/js/users.js): add create user method

--- app/public/js/users.js
+++ app/public/js/users.js
@@ -1,5 +1,7 @@
 const usersModule = (() => {
     const BASE_URL = 'http://localhost:3000/api/v1/users'
+    const headers = new Headers()
+    headers.set('Content-Type', 'application/json')
 
     return {
         fetchAllUsers: async () => {
@@ -17,6 +19,24 @@ const usersModule = (() => {
                               </tr>`
                 document.getElementById('users-list').insertAdjacentHTML('beforeend', body)
             })
+        },
+        createUser: async () => {
+            const name = document.getElementById('name').value
+            const profile = document.getElementById('profile').value
+            const dateOfBirth = document.getElementById('date-of-birth').value
+            const body = {
+                name: name,
+                profile: profile,
+                date_of_birth: dateOfBirth
+            }
+            const res = await fetch(BASE_URL, {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(body)
+            })
+            const resJson = await res.json()
+            alert(resJson.message)
+            window.location.href = '/'
         }
     }
 })()
