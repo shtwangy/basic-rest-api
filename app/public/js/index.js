9db3780a768a7340b24a36bdf53147e72fd7b9ba
shtwangy <shtwangy@gmail.com>
2020-12-15 22:18:23 +0900

    feat(app/public/js/index.js): add edit user page process

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -20,6 +20,22 @@ const indexModule = (() => {
                 return window.location.href = '/'
             })
             break
+        case '/edit.html':
+            const uid = window.location.search.split('?uid=')[1]
+            console.log(uid)
+            document.
+            getElementById('save-btn').addEventListener('click', () => {
+                return usersModule.updateUser(uid)
+            })
+            document.
+            getElementById('cancel-btn').addEventListener('click', () => {
+                return window.location.href = '/'
+            })
+            document.
+            getElementById('delete-btn').addEventListener('click', () => {
+                return usersModule.deleteUser(uid)
+            })
+            return usersModule.setExistingValue(uid)
         default:
             break
     }
